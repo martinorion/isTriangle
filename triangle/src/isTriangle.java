5272dcc03fef7e5ae725c518025d20da2ed3f8b9
MartinHolena <martinkiko18@gmail.com>
2021-09-13 20:48:27 +0200

    is triangle?

---
+++ triangle/src/isTriangle.java
@@ -0,0 +1,18 @@
+public class isTriangle {
+    static void main(String[] args){
+  Triangle(2,5,2);
+    }
+
+    static boolean Triangle(int a, int b, int c){
+    if(a + b > c){
+        return true;
+    }
+    else if (b + c > a){
+        return true;
+    }
+    else if (a + c > b){
+        return true;
+    }
+        return false;
+    }
+}
